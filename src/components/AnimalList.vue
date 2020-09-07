52e93d062c726bfb2d73d4d8cdb7c47fe7ef634d
John Vargas <javargas@gmail.com>
2020-09-06 20:58:28 -0500

    start app

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hello">
+      <v-container fluid>
+      
+        <v-card
+          class="mx-auto"
+          max-width="644"
+          outlined
+          color="#AFcFdF"
+        >
+                  
+    <v-card-text>
+                
+          <div>
+            <v-btn color="primary" @click="loadAnimalList">Domain List</v-btn>  
+          </div>
+
+          <div>
+            <v-data-table
+              :headers="headers"
+              :items="items"
+              :items-per-page="5"
+              class="elevation-1"
+            ></v-data-table>
+          </div>
+          
+    </v-card-text>
+          </v-card>
+      </v-container>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  props: {
+    msg: String
+  },
+    
+  data: function() {
+    return {
+      fields: ['Domain'],
+      items: [],
+
+        headers: [
+          { text: 'name', value: 'id' }
+        ],
+    }
+  },
+
+  methods: {
+    loadAnimalList: function() {
+
+      let url = 'https://api.petfinder.com/v2/animals';
+      console.log("url: ", url);
+
+       axios.get(url ).then(result => { 
+          console.log(result.data) 
+          
+          this.items = result.data.items;
+
+        }).catch( error => {
+            console.error(error);
+            
+      });
+    
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
